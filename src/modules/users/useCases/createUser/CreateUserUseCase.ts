5ba13cd52e8b3ed79e9759314e05aa3a32b0efe3
Weliton <weliton@areacentral.com.br>
2022-03-09 21:27:03 -0300

    All tests concluded

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,26 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userAlreadyExists = this.usersRepository.findByEmail(email);
+        if (userAlreadyExists) {
+            throw new Error("E-mail address is already taken!");
+        }
+
+        const user = this.usersRepository.create({
+            email,
+            name,
+        });
+
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
